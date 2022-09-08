98430630c0f55ecaa7c42cbbe6d5eb769c043e3a
Matthew Jackson <matthew.jackson@lampoo.com>
2022-09-08 17:42:21 +0200

    feat: add store module with tests

---
+++ store/store.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"context"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
